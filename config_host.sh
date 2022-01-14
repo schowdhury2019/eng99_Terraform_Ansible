fa69cc1fa5ab6cb70d42f61e830ee3a3bdf5b3de
schowdhury2019 <sunnychowdhury@hotmail.com>
2022-01-14 13:07:05 +0000

    update hosts with new extension

--- config_host.sh
+++ config_host.sh
@@ -1,2 +1,2 @@
-echo -e "[app]\nec2-instance ansible_host=$(terraform output app_ip) ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/eng99.pem" | sed 's/"//g' > hosts.inv
-echo -e "[db]\nec2-instance ansible_host=$(terraform output db_ip) ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/eng99.pem"  | sed 's/"//g' >> hosts.inv
+echo "[app]\nec2-instance ansible_host=$(terraform output app_ip) ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/eng99.pem" | sed 's/"//g' > hosts.inv
+echo "[db]\nec2-instance ansible_host=$(terraform output db_ip) ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/eng99.pem"  | sed 's/"//g' >> hosts.inv
